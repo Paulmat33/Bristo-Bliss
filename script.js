9facff669dbcc2ba13c5ac3dc27c3a8dad525595
Paulmat33 <paultobi33@gmail.com>
2024-05-30 14:19:19 +0100

    responsiveness update

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ hamburger.onclick = function(){
     navBar = document.querySelector('.nav-bar');
     navBar.classList.toggle('active');
 }
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (this.scrollY >= 100) {
+    toTop.classList.add("active2");
+  } else {
+    toTop.classList.remove("active2");
+  }
+})
+
+
